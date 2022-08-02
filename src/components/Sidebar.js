2ce97b98e2dbec2eea63db3a360972bcfe8cf225
Hanabata <dianneerika.busa@gmail.com>
2022-08-02 21:39:22 +0900

    first commit

---
+++ src/components/Sidebar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+
+function Sidebar(props) {
+    const trash = <FontAwesomeIcon icon={faTrash} color="white" />
+    const notesElements = props.notes.map((note, index) => (
+        <div key={note.id}>
+            <div className={`title ${note.id === props.currentNote.id ? "selected-note" : ""}`}
+                onClick={() => props.setCurrentNoteId(note.id)}>
+                <h4 className='text-snippet'>{note.body.split("\n")[0]}</h4>
+                <button className='delete-btn' onClick={(event) => props.deleteNote(event, note.id)}>{trash}</button>
+            </div>
+        </div >
+    ))
+
+    return (
+        <section className='pane sidebar dark'>
+            <div className='sidebar--header'>
+                <h3>Notes</h3>
+                <button className='new-note' onClick={props.newNote}>+</button>
+            </div>
+            {notesElements}
+        </section>
+    )
+}
+
+export default Sidebar
